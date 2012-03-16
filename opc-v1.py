f9a819b6f55823896d38c3580a743181a58e8357
José Rafael Carrero León <jose.carrero@eseprin.com>
2012-03-16 09:26:21 -0430

    Cambios con las instrucciones de pgsql

--- opc-v1.py
+++ opc-v1.py
@@ -3,7 +3,7 @@ import time
 import calendar
 import ConfigParser
 from optparse import OptionParser
-
+import psycopg2
 
 cfg = ConfigParser.RawConfigParser()
 cfg.read('srv.cfg')
@@ -15,18 +15,29 @@ var = cfg.items('VARIABLES')
 
 #opc = OpenOPC.open_client('host')
 #status_co = opc.connect('server')
-#conn = psycopg2.connect(database)
-#cur = conn.cursor()
+
+db=cfg.get('DATA_BASE_INFO','dbname');
+db_host=cfg.get('DATA_BASE_INFO','host');
+db_user=cfg.get('DATA_BASE_INFO','user');
+db_pass=cfg.get('DATA_BASE_INFO','password');
+
+conn = psycopg2.connect('dbname='+db+' host='+db_host+' user='+db_user+' password='+db_pass)
+cur = conn.cursor()
 
 dato1 = ('100','good','03/15/12 01:00:00')
 dato2 = ('10','bad','03/15/12 10:00:00')
 dato = (dato1,dato2)
-while status_co == True:
+
+"""while status_co == True:
 	for i in range(len(var)):
 		dato = opc.read(var[i][0])
 		n =time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(calendar.timegm(time.strptime(dato[i][2],"%m/%d/%y %H:%M:%S"))))
-	
-		cur.execute("INSERT INTO tabla (var1, var2) VALUES (%s, %s)", (dato[i][0],n[i]))
+"""		
+		if quality == 'Good':
+			cur.execute("INSERT INTO variable_valor (variable,fecha,valor) VALUES ('%s', '%s', %f)", (var[i][0],fecha,valor))
+
+cur.close()
+conn.close()
 
 #value, quality, t = opc.read('MULT_S_Prj_OS(1)::TON-HR-PEND')
 
